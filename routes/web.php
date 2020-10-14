250d98d2666f8b2ba20213ab5759add00d248d0c
pwcodigo <pwcodigo@gmail.com>
2020-10-13 22:33:55 -0300

    feat: codigo inicial - v2 até 7:17

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'adm'], function (){
+    Route::get('/', function(){
+        return "ADM HOME";
+    });
+});
