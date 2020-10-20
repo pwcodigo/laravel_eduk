be7d5dce0def5d8fa7db91502236a08e91f3966a
pwcodigo <pwcodigo@gmail.com>
2020-10-20 19:57:33 -0300

    feat: V5 - finalizado - foi feito rotas do login - v6 prox

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'adm', 'as' => 'adm.'], function (){
     Route::get('/', ['as' => 'index', 'uses' => function(){ return "ADM HOME"; }]);
 });
+
+Route::group(['prefix' => 'account', 'as' => 'account.', 'namespace' => 'Website'], function (){
+    Route::get('/login',['as' => 'login', 'uses' => 'UsersController@login']);
+});
